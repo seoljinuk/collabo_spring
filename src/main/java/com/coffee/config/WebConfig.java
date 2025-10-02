7f657e098d1a92721bd25af6c71db65dc731de5c
bluesky <bluesky@example.com>
2025-10-02 17:03:38 +0900

    spring 2025/10/02

--- src/main/java/com/coffee/config/WebConfig.java
+++ src/main/java/com/coffee/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 3000번 포트에서 GET부터 PATCH까지의 열거한 요청들을 모두 수락하겠습니다.
         registry.addMapping("/**") /* 모든 경로 허용  */
-                .allowedOrigins("http://localhost:3000") /* react 포트 */
+                .allowedOrigins("http://localhost:3100") /* react 포트 */
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")  /* 허용할 메소드 */
                 .allowCredentials(true) ; // 쿠키 전송 허용
     }
