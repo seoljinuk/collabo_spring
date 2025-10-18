d4b171f3fb64c52cfb188001e09ac89cdf8109af
bluesky <bluesky@example.com>
2025-10-18 17:53:34 +0900

    spring boot 2025/10/18

---
+++ src/main/java/com/coffee/service/OrderService.java
@@ -0,0 +1,9 @@
+package com.coffee.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+}
